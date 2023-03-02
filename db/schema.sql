18abaf8e4a7f59839b647d6712a9258c7e88e02d
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2023-03-01 20:29:59 -0500

    Add schema and seed files

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS grades;
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name varchar(255),
+    last_name varchar(255),
+    city varchar(255),
+    company varchar(255),
+    email varchar(255),
+    pic text,
+    skill varchar(255)
+);
+
+CREATE TABLE grades(
+    id SERIAL PRIMARY KEY,
+    score integer DEFAULT 0,
+    student_id integer REFERENCES students(id) ON DELETE CASCADE
+);
+
+CREATE INDEX grades_students_id ON grades(student_id);
